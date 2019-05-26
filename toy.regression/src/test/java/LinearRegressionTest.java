34bdf691b450fa18a47b553e823850196b0bb316
Nick <nick@pop-os.localdomain>
2019-05-25 19:52:27 -0700

    add tests

---
+++ toy.regression/src/test/java/LinearRegressionTest.java
@@ -0,0 +1,24 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class LinearRegressionTest {
+
+    @Test
+    void fitTest() {
+        assertEquals( 1,  1 );
+    }
+
+    @Test
+    void predictTest() {
+        assertEquals( 1,  1 );
+    }
+
+    @Test
+    void scoreTest() {
+        assertEquals( 1,  1 );
+    }
+}
